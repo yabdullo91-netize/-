ac201b4b9857faa65e37b7cb29bce10073b65766
abdulloyakubov <yabdullo91@gmail.com>
2026-06-10 11:51:31 +0500

    Remove old folders from remote and add new folders abdullo, maruf, muhammadsoleh

--- FlowerShop.API.MuhammadsolehNusratzoda/src/FlowerShop.Domain/Entities/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-namespace FlowerShop.Domain.Entities;
-
-public abstract class BaseEntity
-{
-    public int Id { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime? UpdatedAt { get; set; }
-}
--- FlowerShop.API.MuhammadsolehNusratzoda/src/FlowerShop.Domain/Entities/Category.cs
+++
@@ -1,9 +0,0 @@
-namespace FlowerShop.Domain.Entities;
-
-public class Category : BaseEntity
-{
-    public string Name { get; set; } = string.Empty;
-    public string? ImageUrl { get; set; }
-
-    public ICollection<Product> Products { get; set; } = [];
-}
